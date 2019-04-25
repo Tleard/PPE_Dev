0773ac54501995e973604c11d4ce35d1646eeaf6
Gauthier <assal.gauthier@gmail.com>
2019-04-25 10:28:05 +0200

    push affiche note

---
+++ vue/affichageNotes.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Affichage des notes</title>
+        <link rel="stylesheet" href="design/design.css"/>
+    </head>
+    <body>
+        <header>
+            <table class="titre">
+                <tr>
+                    <td><h1>Affichage des notes</h1></td>
+                </tr>
+            </table>
+        </header>
+        <section>
+            <h2>Année 2018/2019</h2>
+            <article>
+                <table class="tableau">
+                <?php
+                    $bdd = new PDO("mysql:host=localhost;dbname=test;charset=utf8", "root", "");
+                    $req = $bdd->query('SELECT * FROM notes');
+                    while ($donnees = $req->fetch())
+                    {
+                        echo "<tr>";
+                        echo "<td>".$donnees['matiere']." : </td>";
+                        echo "<td>".$donnees['note']."</td>";
+                        echo "</tr>";
+                    }
+                ?>
+                </table>
+            </article>
+        </section>
+    </body>
+</html>
