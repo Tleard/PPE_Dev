6e07273a7026997d1809fa0c51edd3aae1ed7a02
Gauthier <assal.gauthier@gmail.com>
2019-04-25 13:31:07 +0200

    bdd

---
+++ modele/ppe.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 25 avr. 2019 à 13:29
+-- Version du serveur :  10.1.30-MariaDB
+-- Version de PHP :  7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ppe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bulletin`
+--
+
+CREATE TABLE `bulletin` (
+  `idProfil` int(11) NOT NULL,
+  `idNote` int(11) NOT NULL,
+  `idMatiere` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matiere`
+--
+
+CREATE TABLE `matiere` (
+  `idMatiere` int(11) NOT NULL,
+  `nomMatiere` varchar(255) NOT NULL,
+  `coef` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `note`
+--
+
+CREATE TABLE `note` (
+  `idNote` int(11) NOT NULL,
+  `note` int(11) NOT NULL,
+  `idMatiere` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profil`
+--
+
+CREATE TABLE `profil` (
+  `idProfil` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `rang` tinyint(1) NOT NULL DEFAULT '1',
+  `login` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `section` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `referent`
+--
+
+CREATE TABLE `referent` (
+  `idProfil` int(11) NOT NULL,
+  `idMatiere` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bulletin`
+--
+ALTER TABLE `bulletin`
+  ADD PRIMARY KEY (`idProfil`,`idNote`),
+  ADD KEY `idNote` (`idNote`),
+  ADD KEY `idMatiere` (`idMatiere`);
+
+--
+-- Index pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  ADD PRIMARY KEY (`idMatiere`);
+
+--
+-- Index pour la table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`idNote`),
+  ADD KEY `matiere` (`idMatiere`),
+  ADD KEY `idMatiere` (`idMatiere`),
+  ADD KEY `idMatiere_2` (`idMatiere`),
+  ADD KEY `idMatiere_3` (`idMatiere`);
+
+--
+-- Index pour la table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`idProfil`);
+
+--
+-- Index pour la table `referent`
+--
+ALTER TABLE `referent`
+  ADD PRIMARY KEY (`idProfil`,`idMatiere`),
+  ADD KEY `idMatiere` (`idMatiere`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  MODIFY `idMatiere` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `note`
+--
+ALTER TABLE `note`
+  MODIFY `idNote` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `idProfil` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
