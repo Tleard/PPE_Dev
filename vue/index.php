3175e57f4f8e31c5af0fc9b1f3546f344a2953e3
Thomas <thomas.leard@gmail.com>
2019-05-05 16:59:50 +0200

    Login + Deconnexion Done
    Maybe a few front-end error to fix

--- vue/index.php
+++ vue/index.php
@@ -12,39 +12,21 @@
         <h1>Bienvenue sur le site du PPE</h1>
         <br/>
 
-        <form class="text-center border border-light p-5 col-md-4 col-md-offset-4" style="margin-left: 33%">
+            <?php
+            if (isset($_SESSION['id'])){
+             echo 'Bienvenue' . ' '. $_SESSION['prenom'] .' '. $_SESSION['nom'];
 
-            <p class="h4 mb-4">Connexion</p>
-
-            <input type="text" name="ndc" id="ndc" maxlength=20 class="form-control mb-4" placeholder="Votre pseudo(max20caract.)"/>
-            <input type="password" name="mdp" id="mdp" maxlength=15 placeholder="*****" class="form-control mb-4" />
-
-
-            <!-- Sign in button -->
-            <button class="btn btn-info btn-block my-4" value="connexion" type="submit" style="margin: 0">Sign in</button>
-
-            <!-- Register -->
-                <a href="account.php">Inscription</a>
-
-            </form>
+             echo '<a href="deconnexion.php">
+            Deconnexion
+            </a>';
+            }else
+                include 'formlogin.php'
+            ?>
 
 
-        <!--<form class="form-signin">
-            <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-
-            <input type="text" name="ndc" id="ndc" maxlength=20 class="form-control" placeholder="Votre pseudo(max20caract.)"/>
-            <input type="password" name="mdp" id="mdp" maxlength=15 placeholder="*****" class="form-control" />
-            <button type="submit" value="connexion" class="btn btn-lg btn-primary btn-block"> Connecter vous</button>
-
-            <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-        </form>
-        <a href="account.php"><p>Créez un compte</p></a>-->
 
 
-        <article>
-            <p> Rien n'est fait, tout reste à faire. </p>
-        </article>
+
     </section>
     <?php
     include "footer.inc.php"
