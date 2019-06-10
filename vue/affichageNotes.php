7e51bcbada92b44038c48880a5624f2e42bc6b9e
Thomas <thomas.leard@gmail.com>
2019-06-10 13:24:01 +0200

    Datatable for notes + View connect.php Fix

--- vue/affichageNotes.php
+++ vue/affichageNotes.php
@@ -6,34 +6,71 @@ include ('../modele/connectDB.php');
 <?php
 include ('header.inc.php');
 ?>
-    <body>
-        <header>
-            <table class="titre">
-                <tr>
-                    <td><h1>Affichage des notes</h1></td>
-                </tr>
-            </table>
-        </header>
-        <section>
-            <h2>Année 2018/2019</h2>
-            <article>
-                <table class="table">
-                <?php
-                $array_notes = $DB->query("SELECT matiere.nomMatiere, note.note
-                from matiere INNER JOIN note on matiere.idMatiere = note.idMatiere INNER JOIN profil ON note.idProfil = profil.id
-                WHERE profil.id = ?", array($_SESSION['id']));
-                $array_notes = $array_notes->fetchAll();
-                foreach($array_notes as $an) {
-
-                    echo "<tr> <td>" . $an['note'] . "</td>";
-                    echo"<td>". $an['nomMatiere'] ."</td>";
-                    echo"</tr>";
-                }
-                ?>
-
-                </table>
-            </article>
-        </section>
     <?php
+    /*$array_notes = $DB->query("SELECT matiere.idMatiere, matiere.nomMatiere, note.note
+                    from matiere INNER JOIN note on matiere.idMatiere = note.idMatiere INNER JOIN profil ON note.idProfil = profil.id
+                    WHERE profil.id = ?", array($_SESSION['id']));
+    $array_notes = $array_notes->fetchAll();*/
+    ?>
+    <!--Matiere Specifique requette-->
+    <?php
+        $id_matiere = 1;
+        $array_notes = $DB->query("SELECT matiere.idMatiere, matiere.nomMatiere, note.note
+                        from matiere INNER JOIN note on matiere.idMatiere = note.idMatiere INNER JOIN profil ON note.idProfil = profil.id
+                        WHERE profil.id = ? AND matiere.idMatiere = ?", array($_SESSION['id'], $id_matiere));
+        $array_notes = $array_notes->fetchAll();
+    ?>
+        <h1 class="text-center">Affichage des notes</h1></>
+        <section>
+            <h2 class="text-center">Année 2018/2019</h2>
+
+            <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                <tr>
+                    <th class="th-sm">Nom matiére</th>
+                    <th class="th-sm">Notes</th>
+                    <th class="th-sm">Moyenne</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <?php
+                    /** Si les notes ont le même Id l'insérér dans une string commune,
+                     * Determiner le nombre de string néccésaires avec un parseur qui fait toutes les notes
+                     */
+
+                    /**
+                     * Determine le nombre de matiére pour l'affichage
+                     */
+                    foreach($array_notes as $an) {
+                        $id_matiere = 1;
+                        $tem = $an['idMatiere'];
+                        if ($tem = $an['idMatiere'])
+                        {
+                            $nb_matiere = $nb_matiere + 1;
+                            $tem = $an['idMatiere'];
+                        }
+                        while ($id_matiere < $nb_matiere)
+                        {
+                                if ($id_matiere == $an['idMatiere'])
+                                {
+                                    echo($an['idMatiere']);
+                                    echo "<tr> <td>" . $an['nomMatiere'] . "</td>";
+                                    echo "<td>" . $an['note'] . "</td>";
+                                    echo "<td>" . $an['note'] . "</td>";
+                                    echo "</tr>";
+                                    $id_matiere++;
+                                }
+                        }
+                    }
+
+                    ?>
+                </tr>
+                </tbody>
+            </table>
+        </section>
+
+
+<?php
     include('footer.inc.php')
     ?>
--- vue/connect.php
+++ vue/connect.php
@@ -3,20 +3,20 @@
     <br/>
     <div class="container">
         <div class="row">
-            <div class="col-md-7 ">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">  <h4 >Vos informations</h4></div>
                     <div class="panel-body">
                         <div class="box box-info">
                             <div class="box-body">
-                                <div class="col-sm-6">
+                                <div class="col-sm-12">
                                     <div  align="center"> <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive">
                                         <input id="profile-image-upload" class="hidden" type="file">
                                         <div style="color:#999;" >Cliquez ici pour changer votre photo de profil</div>
                                     </div>
                                     <br>
                                 </div>
-                                <div class="col-sm-6">
+                                <div class="col-sm-12">
                                     <h4 style="color:#00b1b1;"><?php echo $_SESSION['nom'] . ' ' . $_SESSION['prenom'] . ' <br/> ';
                                         echo '<a href="deconnexion.php">Deconnexion</a>'; ?> </h4>
                                 </div>
@@ -24,34 +24,34 @@
                                 <hr style="margin:5px 0 5px 0;">
 
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre nom d'utilisateur:</div><div class="col-sm-7 col-xs-6 "><?php echo $_SESSION['login']?></div>
+                                <div class="col-sm-12" >Votre nom d'utilisateur:</div><div class="col-sm-12 "><?php echo $_SESSION['login']?></div>
                                 <div class="clearfix"></div>
                                 <div class="bot-border"></div>
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre nom:</div><div class="col-sm-7"><?php echo $_SESSION['nom']?></div>
+                                <div class="col-sm-12 tital " >Votre nom:</div><div class="col-sm-12"><?php echo $_SESSION['nom']?></div>
                                 <div class="clearfix"></div>
                                 <div class="bot-border"></div>
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre prénom:</div><div class="col-sm-7"><?php echo $_SESSION['prenom']?></div>
+                                <div class="col-sm-12 tital " >Votre prénom:</div><div class="col-sm-12"><?php echo $_SESSION['prenom']?></div>
                                 <div class="clearfix"></div>
                                 <div class="bot-border"></div>
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre adresse mail:</div><div class="col-sm-7"><?php echo $_SESSION['mail']?></div>
+                                <div class="col-sm-12 tital " >Votre adresse mail:</div><div class="col-sm-12"><?php echo $_SESSION['mail']?></div>
 
                                 <div class="clearfix"></div>
                                 <div class="bot-border"></div>
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre classe:</div><div class="col-sm-7"><?php echo $_SESSION['classe']?></div>
+                                <div class="col-sm-12 tital " >Votre classe:</div><div class="col-sm-12"><?php echo $_SESSION['classe']?></div>
 
                                 <!--<div class="clearfix"></div>
                                     <div class="bot-border"></div>
 
-                                    <div class="col-sm-5 col-xs-6 tital " >Compte crée depuis:</div><div class="col-sm-7"><?php echo $_SESSION['login']?></div>-->
+                                    <div class="col-sm-12 tital " >Compte crée depuis:</div><div class="col-sm-12"><?php echo $_SESSION['login']?></div>-->
 
                                 <div class="clearfix"></div>
                                 <div class="bot-border"></div>
 
-                                <div class="col-sm-5 col-xs-6 tital " >Votre rang:</div><div class="col-sm-7">
+                                <div class="col-sm-12 tital " >Votre rang:</div><div class="col-sm-12">
                                     <?php
                                     if($_SESSION['rang'] == 0){
                                         echo 'En attente.<br>';
@@ -83,7 +83,7 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-7 ">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">  <h4 >Modifier vos informations</h4></div>
                     <div class="panel-body">
@@ -93,24 +93,24 @@
                                 <hr style="margin:5px 0 5px 0;">
 
                                 <form method="post" action="../modele/update.php">
-                                    <div class="col-sm-5 col-xs-6 tital " >Changer votre nom d'utilisateur</div><div class="col-sm-7 col-xs-6 "><input name="newlogin" id="newlogin" type="text" /></div>
+                                    <div class="col-sm-12 tital " >Changer votre nom d'utilisateur</div><div class="col-sm-12 "><input name="newlogin" id="newlogin" type="text" /></div>
                                     <div class="clearfix"></div>
                                     <div class="bot-border"></div>
 
-                                    <div class="col-sm-5 col-xs-6 tital " >Changer votre adresse mail</div><div class="col-sm-7"><input name="newadress" id="newadress" type="text" /></div>
+                                    <div class="col-sm-12 tital " >Changer votre adresse mail</div><div class="col-sm-12"><input name="newadress" id="newadress" type="text" /></div>
                                     <div class="clearfix"></div>
                                     <div class="bot-border"></div>
 
-                                    <div class="col-sm-5 col-xs-6 tital " >Changer votre mot de passe</div><div class="col-sm-7"><input name="newpassword" id="newpassword" type="text" /></div>
+                                    <div class="col-sm-12 tital " >Changer votre mot de passe</div><div class="col-sm-12"><input name="newpassword" id="newpassword" type="text" /></div>
                                     <div class="clearfix"></div>
                                     <div class="bot-border"></div>
 
-                                    <div class="col-sm-5 col-xs-6 tital " >Vérifiez votre mot de passe</div><div class="col-sm-7"><input name="newpassword2" id="newpassword2" type="text" /></div>
+                                    <div class="col-sm-12 tital " >Vérifiez votre mot de passe</div><div class="col-sm-12"><input name="newpassword2" id="newpassword2" type="text" /></div>
 
                                     <div class="clearfix"></div>
                                     <div class="bot-border"></div>
 
-                                    <div class="col-sm-5 col-xs-6 tital " ></div><div class="col-sm-7"><input name="postchange" id="postchange" type="submit" /></div>
+                                    <div class="col-sm-12 tital " ></div><div class="col-sm-12"><input name="postchange" id="postchange" type="submit" /></div>
                                 </form>
                             </div>
                         </div>
